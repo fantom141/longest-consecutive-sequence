83bcd33f8fb3abaafcc4b8a27c8e3fae6bdf8e9b
Alexander Balan <fantom141141@gmail.com>
2018-10-03 01:58:23 +0300

    longest consecutive length

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
-module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+module.exports = function longestConsecutiveLength(a) {
+
+    if (!a.length) {
+        return 0;
+    }
+
+    var s = [];
+    var m = 1;
+
+    for (var i=0; i<a.length; i++)
+        s.push(a[i]);
+
+    for (var i=0; i<a.length; i++) {
+        var l = a[i] - 1;
+        var r = a[i] + 1;
+        var c = 1;
+
+        while (s.includes(l)) {
+            c++;
+            s.splice(s.indexOf(l), 1);
+            l--;
+        }
+
+        while (s.includes(r)) {
+            c++;
+            s.splice(s.indexOf(r), 1);
+            r++;
+        }
+
+        m = Math.max(c, m);
+    }
+
+    return m;
 }
